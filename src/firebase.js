07117e08afa70f441659bb8f95fadf2c83c96065
Murat Sahin <yazilimogren1111@gmail.com>
2021-04-01 23:27:53 +0200

    project started

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8OJdWjAjukLf--3Rn4oFZ40LTraic0C4",
+  authDomain: "linkedin-efedb.firebaseapp.com",
+  projectId: "linkedin-efedb",
+  storageBucket: "linkedin-efedb.appspot.com",
+  messagingSenderId: "890879272337",
+  appId: "1:890879272337:web:74aa083b3ab31e06a72630"
+};
